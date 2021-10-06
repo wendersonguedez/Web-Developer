adf622e4d913ba2569961fd4e513b42656265cf7
wenderson guedes <wendersonguedes6@gmail.com>
2021-10-06 20:04:01 -0300

    Formatardata brasileira para data americana

---
+++ PHP/Exercicios/DataBR-paraDataUS.php
@@ -0,0 +1,18 @@
+<?php
+// EFETUANDO A FORMATAÇÃO DE DATA BRASILEIRA E EXIBINDO A MESMA.
+$formatoBR = mktime(12, 0, 0, 12, 31, 2010); 
+echo "Formato brasileiro: " . date("d/m/Y H:i:s", $formatoBR);
+
+// QUEBRA DE LINHA
+echo "<br><br>";
+
+// CHAMANDO A FUNÇÃO PARA REALIZAR A FORMATAÇÃO E EXIBINDO ELA.
+$formatoUS = dataUS();
+echo "Formato Americano: " . $formatoUS;
+
+// FUNÇÃO PARA FORMATAR UMA DATA NO FORMATO AMERICANO.
+function dataUS(){
+    $formatoUS = date("Y-m-d H:i:s", mktime(12, 0, 0, 12, 31, 2010));
+    return $formatoUS;
+}
+?>
